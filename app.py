eba95eab1bcfb996ad3f39ba803ae1dd51d50837
Mateus Morais <mateus.batista010@gmail.com>
2024-06-26 19:13:32 +0000

    [feat]: my first online ml classifier model

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+import joblib
 
 app = Flask(__name__)
+model = joblib.load('text_classifier.pkl')
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    text = request.json['text']
+    category = model.predict([text])[0]
+    return jsonify({'category': category})
+
+if __name__ == '__main__':
+    app.run(debug=True)
